8bee328ec4b9ae5c0d76e3cf25323080817955b7
Xeniia <vorobyeva.ksenya@gmail.com>
2020-06-06 23:37:40 +0700

    reply_to to send_message subtitution

--- ourbot.py
+++ ourbot.py
@@ -5,14 +5,14 @@ bot = telebot.TeleBot('1042781969:AAH9kxoyrHi6MaXWLT3ER9d11hQmP9mJ8u8')
 @bot.message_handler(content_types=['text'])
 def handle_text_messages(message):
     if message.text == "кись":
-        #bot.send_message(message.from_user.id, "Серый-серый-серый-серый-серенький Кись!")
-        #bot.send_message(message.from_user.id, "У него всё очень-очень хорошо!")
-        bot.reply_to(message, "Серый-серый-серый-серый-серенький Кись!")
-        bot.reply_to(message, "У него всё очень-очень хорошо!")
+        bot.send_message(message.from_user.id, "Серый-серый-серый-серый-серенький Кись!")
+        bot.send_message(message.from_user.id, "У него всё очень-очень хорошо!")
+        #bot.reply_to(message, "Серый-серый-серый-серый-серенький Кись!")
+        #bot.reply_to(message, "У него всё очень-очень хорошо!")
     elif message.text == "мяу":
-        #bot.send_message(message.from_user.id, "мяу")
-        bot.reply_to(message, "мяу")
+        bot.send_message(message.from_user.id, "мяу")
+        #bot.reply_to(message, "мяу")
     else:
-        #bot.send_message(message.from_user.id, "Напиши 'кись' чтобы узнать как дела у серого кися")
-        bot.reply_to(message, "Напиши 'кись' чтобы узнать как дела у серого кися")
+        bot.send_message(message.from_user.id, "Напиши 'кись' чтобы узнать как дела у серого кися")
+        #bot.reply_to(message, "Напиши 'кись' чтобы узнать как дела у серого кися")
 bot.polling(none_stop=True, interval=0)
