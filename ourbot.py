49c806416b7e3586fd42ec081b294a66be9de2c8
Xeniia <vorobyeva.ksenya@gmail.com>
2020-06-06 23:10:30 +0700

    add myaoo in the swich

--- ourbot.py
+++ ourbot.py
@@ -7,6 +7,8 @@ def handle_text_messages(message):
     if message.text == "кись":
         bot.send_message(message.from_user.id, "Серый-серый-серый-серый-серенький Кись!")
         bot.send_message(message.from_user.id, "У него всё очень-очень хорошо!")
+    elif message.text == "мяу":
+        bot.send_message(message.from_user.id, "мяу")
     else:
-        bot.send_message(message.from_user.id, "Напиши 'кись' чтобы узнать как дела у серого кися" )
+        bot.send_message(message.from_user.id, "Напиши 'кись' чтобы узнать как дела у серого кися")
 bot.polling(none_stop=True, interval=0)
