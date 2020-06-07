07fbf355f239ca5fcfb40b7a2c5a1da25e13989f
Xeniia <vorobyeva.ksenya@gmail.com>
2020-06-07 18:39:48 +0700

    trying on help command

--- ourbot.py
+++ ourbot.py
@@ -23,8 +23,8 @@ def handle_text_messages(message):
         #bot.send_sticker(message.chat.id, "CAACAgIAAxkBAALo817b0dwV_AZwTyvKYd4n5Vjv9xKKAAJ6AANb-bUSLcB_10CpHboaBA")
         bot.send_voice(message.chat.id, "AwACAgIAAxkBAAIBQF7ceKypMg7g0p274vklpkb0oOAZAAJVCAAC81vhSrWrdOpc_t76GgQ")
 
-    else:
-        bot.send_message(message.chat.id, "Напиши 'спой' чтобы послушать одну из песенок про серого кися")
+    #else:
+        #bot.send_message(message.chat.id, "Напиши 'спой' чтобы послушать одну из песенок про серого кися")
         #bot.reply_to(message, "Напиши 'кись' чтобы узнать как дела у серого кися")
 bot.polling(none_stop=True, interval=0)
 
