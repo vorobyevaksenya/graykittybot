b8c1ee995efd21467523a330dab37b74ff1278d4
Xeniia <vorobyeva.ksenya@gmail.com>
2020-06-07 18:58:17 +0700

    trying on about command

--- ourbot.py
+++ ourbot.py
@@ -2,10 +2,10 @@ import telebot
 import re
 import random
 
-
 bot = telebot.TeleBot('1042781969:AAH9kxoyrHi6MaXWLT3ER9d11hQmP9mJ8u8')
 
 @bot.message_handler(content_types=['text'])
+
 def handle_text_messages(message):
 
     if re.search("спой", message.text, re.IGNORECASE):
@@ -23,8 +23,13 @@ def handle_text_messages(message):
         #bot.send_sticker(message.chat.id, "CAACAgIAAxkBAALo817b0dwV_AZwTyvKYd4n5Vjv9xKKAAJ6AANb-bUSLcB_10CpHboaBA")
         bot.send_voice(message.chat.id, "AwACAgIAAxkBAAIBQF7ceKypMg7g0p274vklpkb0oOAZAAJVCAAC81vhSrWrdOpc_t76GgQ")
 
-    #else:
-        #bot.send_message(message.chat.id, "Напиши 'спой' чтобы послушать одну из песенок про серого кися")
+    else:
+        bot.send_message(message.chat.id, "Напиши 'спой' чтобы послушать одну из песенок про серого кися")
         #bot.reply_to(message, "Напиши 'кись' чтобы узнать как дела у серого кися")
 bot.polling(none_stop=True, interval=0)
 
+
+@bot.message_handler(commands=['about_bot'])
+def about_bot(message):
+    bot.send_message(message.chat.id, "Я создала этого бота, чтобы Лехе не было грустненько, и он мог прослушать песенки про серого кися")
+bot.polling(none_stop=True, interval=0)
