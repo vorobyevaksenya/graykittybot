36bc5cac9c6c8aa36950e91ec579d36acc08a0e1
Xeniia <vorobyeva.ksenya@gmail.com>
2020-06-07 13:46:12 +0700

    add regexp for myaoo

--- ourbot.py
+++ ourbot.py
@@ -1,4 +1,5 @@
-import telebot 
+import telebot
+import re
 
 bot = telebot.TeleBot('1042781969:AAH9kxoyrHi6MaXWLT3ER9d11hQmP9mJ8u8')
 
@@ -8,7 +9,9 @@ def handle_text_messages(message):
     if message.text == "кись":
         bot.send_message(message.chat.id, "Серый-серый-серый-серый-серенький Кись!\nУ него всё очень-очень хорошо!")
         #bot.reply_to(message, "Серый-серый-серый-серый-серенький Кись!\nУ него всё очень-очень хорошо!")
-    elif message.text == "мяу":
+    elif message.text == re.compile("мяу"):
+        bot.send_message(message.chat.id, "применилась регулярка")
+    elif message.text == "мяу1":
         #bot.send_message(message.from_user.id, "мяу")
         #bot.reply_to(message, "мяу")
         #bot.send_sticker(message.chat.id, "CAACAgIAAxkBAALo817b0dwV_AZwTyvKYd4n5Vjv9xKKAAJ6AANb-bUSLcB_10CpHboaBA")
@@ -18,3 +21,4 @@ def handle_text_messages(message):
         bot.send_message(message.chat.id, "Напиши 'кись' чтобы узнать как дела у серого кися")
         #bot.reply_to(message, "Напиши 'кись' чтобы узнать как дела у серого кися")
 bot.polling(none_stop=True, interval=0)
+
